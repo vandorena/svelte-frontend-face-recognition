d7f3f5a5e132f5323fb9b286d5937e867b23cb95
vandorena <alexvd@hackclub.com>
2026-02-28 10:21:38 -0500

    added first webcam streaming!!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  let video_source = $state(null);
+  let loading = $state(false);
+  const webcam_video = async () => {
+    try {
+      loading = true;
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+      });
+      video_source.srcObject = stream;
+      video_source.play();
+      loading = false;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+</script>
+
+<div>
+  {#if loading}
+    <h1>LOADING</h1>
+  {/if}
+  <video bind:this={video_source}></video>
+  <button onclick={webcam_video}>CLICK</button>
+</div>
